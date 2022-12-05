bf7fd7ed05a282cd1bde4cde48d9cc31e89c16f7
EDB02 <edalbianco02@gmail.com>
2022-12-05 18:09:46 +0100

    fixed test

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,12 @@
 #include "ws_connection.h"
 #include "connection.h"
 #include "mqtt_connection.h"
+#include <iostream>
+
 
 int main()
 {
+    MQTTConnection tmp("sube", "melearadio");
+    std::cout<<tmp.getPendingMessages()<<"\n";
     return 0;
 }
