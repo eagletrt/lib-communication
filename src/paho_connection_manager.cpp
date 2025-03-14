8e034096c6ec8a58084106db31d5a671147a4009
StefanoSacchet <sacchet13@gmail.com>
2025-03-14 10:55:15 +0100

    feat: weak_pointer for connection manager

--- src/paho_connection_manager.cpp
+++ src/paho_connection_manager.cpp
@@ -2,13 +2,14 @@
 
 #include <atomic>
 #include <condition_variable>
+#include <memory>
 #include <mutex>
 #include <paho_mqtt_connection.hpp>
 #include <thread>
 #include <vector>
 
 namespace PAHOConnectionManager {
-std::vector<PAHOMQTTConnection *> connections;
+std::vector<std::weak_ptr<PAHOMQTTConnection>> connections;
 std::unique_ptr<std::thread> connectionThread = NULL;
 std::mutex connectionMutex;
 std::condition_variable connectionCondition;
@@ -18,12 +19,21 @@ static void connectionThreadFunction() {
   connectionThreadRunning = true;
   while (connectionThreadRunning.load()) {
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    auto conns = connections;
+    connections.clear();
+
     std::unique_lock<std::mutex> lck(connectionMutex);
-    for (auto connection : connections) {
-      if (connection->getStatus() == PAHOMQTTConnectionStatus::CONNECTED ||
-          connection->getStatus() == PAHOMQTTConnectionStatus::CONNECTING)
-        continue;
-      connection->connect();
+    for (auto weak_conn : connections) {
+      if (auto connection = weak_conn.lock()) {
+        if (connection->getStatus() == PAHOMQTTConnectionStatus::CONNECTED ||
+            connection->getStatus() == PAHOMQTTConnectionStatus::CONNECTING) {
+          connections.push_back(connection);
+          continue;
+        }
+        connection->connect();
+        connections.push_back(connection);
+      }
     }
   }
 }
@@ -42,21 +52,27 @@ void stop() {
     connectionThread->join();
 }
 
-bool addConnection(PAHOMQTTConnection *connection) {
+bool addConnection(std::shared_ptr<PAHOMQTTConnection> connection) {
   std::unique_lock<std::mutex> lck(connectionMutex);
-  for (const auto *conn : connections)
-    if (conn == connection)
-      return false;
+  for (const auto &weak_conn : connections) {
+    if (auto conn = weak_conn.lock()) {
+      if (conn == connection) {
+        return false;
+      }
+    }
+  }
   connections.push_back(connection);
   return true;
 }
 
-bool removeConnection(PAHOMQTTConnection *connection) {
+bool removeConnection(std::shared_ptr<PAHOMQTTConnection> connection) {
   std::unique_lock<std::mutex> lck(connectionMutex);
-  for (int i = 0; i < connections.size(); i++) {
-    if (connections[i] == connection) {
-      connections.erase(connections.begin() + i);
-      return true;
+  for (auto it = connections.begin(); it != connections.end(); ++it) {
+    if (auto conn = it->lock()) {
+      if (conn == connection) {
+        connections.erase(it);
+        return true;
+      }
     }
   }
   return false;
@@ -64,13 +80,19 @@ bool removeConnection(PAHOMQTTConnection *connection) {
 
 void connect_all() {
   std::unique_lock<std::mutex> lck(connectionMutex);
-  for (auto connection : connections)
-    connection->connect();
+  for (auto weak_conn : connections) {
+    if (auto connection = weak_conn.lock()) {
+      connection->connect();
+    }
+  }
 }
 
 void disconnect_all() {
   std::unique_lock<std::mutex> lck(connectionMutex);
-  for (auto connection : connections)
-    connection->disconnect();
+  for (auto weak_conn : connections) {
+    if (auto connection = weak_conn.lock()) {
+      connection->disconnect();
+    }
+  }
 }
 } // namespace PAHOConnectionManager
